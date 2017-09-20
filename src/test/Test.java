ba038f926ea8c69ece8f170b82f47849dcd59c92
yoshi-bow <shinichi_yoshino@hotmail.com>
2017-09-20 14:45:41 +0900

    test

---
+++ src/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package test;
+
+/**
+ * @author S.Yoshino
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
